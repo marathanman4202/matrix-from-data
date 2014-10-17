ae90bb8b4c0fb0bdc4fdbc5e65dbb74f0a53a14f
marathonman4202 <roy.haggerty@oregonstate.edu>
2014-10-17 09:54:06 -0700

    Added capability to read xls

--- matrix_from_xls.py
+++ matrix_from_xls.py
@@ -1,20 +1,23 @@
-def matrix_from_xls(file_w_path,column,xcycle,skip):
+def matrix_from_xls(file_w_path,column,xcycle,skip,filetype='csv'):
     """Reads csv file and produces 2-D matrix
     """
     import numpy as np
-    
-    data_tmp = np.array(np.genfromtxt(file_w_path, delimiter=',',skip_header=1)) # Read csv file
-    data_yr_tmp = data_tmp[:,column]
+    import xlrd
+
+    if filetype == 'csv':
+        data_tmp = np.array(np.genfromtxt(file_w_path, delimiter=',',skip_header=1)) # Read csv file
+        data_yr_tmp = data_tmp[:,column]
+    elif filetype == 'xls':
+        workbook = xlrd.open_workbook(file_w_path)
+        # get 0th sheet, column, starting at 1st fow
+        sheetnum = 0
+        rowstart = 1
+        data_yr_tmp = np.array(workbook.sheet_by_index(sheetnum).col_values(column)[rowstart:])
+
     numdat = len(data_yr_tmp)
 #    data_yr = data_yr_tmp[skip:-(xcycle-skip+numdat%xcycle)] # start at skip
     data_yr = data_yr_tmp[skip:-((numdat-skip)%xcycle)] # start at skip + 1 and go as close to end of data as possible
     data_2D = np.reshape(np.array(data_yr), (-1,xcycle)) #2D matrix of data in numpy format
-    
-    print 'whole eqn ', xcycle-skip+numdat%xcycle 
-    print 'xcycle ',xcycle
-    print 'skip ',skip
-    print 'numdat ',numdat
-    print 'numdat%xcycle ', numdat%xcycle
     
     
     return data_2D
--- test.py
+++ test.py
@@ -7,9 +7,9 @@ Created on Thu Oct 16 21:49:56 2014
 
 from matrix_from_xls import matrix_from_xls as mfx
 
-file = 'C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\Cascade_plots\\save tmp\\testdata.csv'
+file = 'C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\Cascade_plots\\save tmp\\testdata.xls'
 
-a = mfx(file,1,24,0)
+a = mfx(file,1,24,3,filetype='xls')
 
 b=a
 
