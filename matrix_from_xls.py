b1f3ac997f3da7429b9880425f81f0263390df09
marathanman4202 <roy.haggerty@oregonstate.edu>
2014-10-17 14:41:44 -0700

    added ability to read from google spreadsheet

--- matrix_from_xls.py
+++ matrix_from_xls.py
@@ -1,10 +1,19 @@
 def matrix_from_xls(file_w_path,column,xcycle,skip,filetype='csv'):
-    """Reads csv file and produces 2-D matrix
+    """
+    Reads sheet (excel, google, csv) file and produces 2-D matrix
+    Returns 2D numpy array
+    
+    file_w_path = filename and location of file. If google sheet, then key
+    column = column number for data
+    xcycle = how many numbers in each row
+    skip = how many numbers to skip before starting to read data
+    filetype = type of file
     """
     import numpy as np
     import xlrd
     
-    filetype = file_w_path.rsplit('.')[-1]
+    if filetype != 'gsheet':  # unless it is a google sheet, get filetype from windows extension
+        filetype = file_w_path.rsplit('.')[-1]
 
     if filetype == 'csv':
         data_tmp = np.array(np.genfromtxt(file_w_path, delimiter=',',skip_header=1)) # Read csv file
@@ -15,9 +24,18 @@ def matrix_from_xls(file_w_path,column,xcycle,skip,filetype='csv'):
         sheetnum = 0
         rowstart = 1
         data_yr_tmp = np.array(workbook.sheet_by_index(sheetnum).col_values(column)[rowstart:])
+    elif filetype == 'gsheet':
+        import imp
+        import sys
+        sys.path.append('c:\\code\\gspread\\')  # location of gspread module
+        import gspread  # gspread is available at https://github.com/burnash/gspread
+        ui = imp.load_source('userinfo', 'C:\\keys\\userinfo.py')
+        gc = gspread.login(ui.userid,ui.pw)
+        sheet = gc.open_by_key(file_w_path).sheet1
+        data_str = np.array(sheet.get_all_values())
+        data_yr_tmp = data_str[1:,column].astype(np.float)
 
     numdat = len(data_yr_tmp)
-#    data_yr = data_yr_tmp[skip:-(xcycle-skip+numdat%xcycle)] # start at skip
     data_yr = data_yr_tmp[skip:-((numdat-skip)%xcycle)] # start at skip + 1 and go as close to end of data as possible
     data_2D = np.reshape(np.array(data_yr), (-1,xcycle)) #2D matrix of data in numpy format
     
--- test.py
+++ test.py
@@ -7,9 +7,11 @@ Created on Thu Oct 16 21:49:56 2014
 
 from matrix_from_xls import matrix_from_xls as mfx
 
-file = 'C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\Cascade_plots\\save tmp\\testdata.xls'
+#file = 'C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\Cascade_plots\\save tmp\\testdata.xls'
+#file = 'C:\\code\\matrix-from-data\\testdata.xls'
+file = '1e__IjqMLBBVLqBHWZiIORkPbz6W8PQlSPVeodQBM8Oc'  #google key
 
-a = mfx(file,1,24,3,filetype=typef)
+a = mfx(file,1,24,7,filetype='gsheet')
 
 b=a
 
